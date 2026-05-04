b7fb65f04702aa0a8a9ddbcf86a3a415948849fa
Andreas <andreasjonsson00@gmail.com>
2026-05-04 10:20:29 +0200

    add favorite feature

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,13 @@
-const Favorites = () => {
-  return <div>Favorites</div>;
+import Pokemon from "../components/Pokemon";
+
+const Favorites = ({ favorites }) => {
+  return (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {favorites.map((p) => (
+        <Pokemon key={p.id} pokemon={p} />
+      ))}
+    </div>
+  );
 };
 
 export default Favorites;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import { getAllPokemon } from "../api/dataApi";
 import Pokemon from "../components/Pokemon";
 
-const Home = () => {
+const Home = ({setFavorites}) => {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,14 +31,21 @@ const Home = () => {
     return <p className="font-bold p-5">Error: {error}</p>;
   }
 
+  const addFavorite = (pokemon) => {
+    setFavorites((prev) => {
+      if (prev.find((p) => p.id === pokemon.id)) return prev;
+      return [...prev, pokemon];
+    });
+  };
+
   return (
     <div>
       <h1 className="text-2xl font-bold text-center mt-6">
         Add your favorite Pokémon
       </h1>
-      <div className="pokemon-container mt-4 flex flex-wrap justify-center gap-4 border p-4 rounded-lg bg-gray-100 m-4">
+      <div className="pokemon-container mt-4 flex flex-wrap justify-center gap-4 border p-4 rounded-lg bg-gray-100 m-17">
         {pokemon.map((p) => (
-          <Pokemon key={p.id} pokemon={p} />
+          <Pokemon key={p.id} pokemon={p} onAddFavorite={addFavorite} />
         ))}
       </div>
       <h2 className="text-2xl font-bold text-center mt-6">
