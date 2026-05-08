cba133b5b537ee0e2b6a801ec820e1ab7e070c37
Andreas <andreasjonsson00@gmail.com>
2026-05-08 10:40:09 +0200

    style change

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -3,30 +3,32 @@ import { Link } from "react-router-dom";
 const Pokemon = ({ pokemon, onAddFavorite, onRemoveFavorite }) => {
   return (
     <div className="p-4 border rounded-lg bg-white shadow-md flex flex-col items-center transition transform hover:scale-105 hover:shadow-xl">
-     <Link to={`/pokemon/${pokemon.id}`} className="text-center">
-      <img
-        src={pokemon.sprites.front_default}
-        alt={pokemon.name}
-        className="w-32 h-32"
-      />
-      <h2 className="capitalize mt-2 font-bold">{pokemon.name}</h2>
-     </Link>
-      {onAddFavorite && (
-        <button
-          onClick={() => onAddFavorite(pokemon)}
-          className="hover:underline mt-2 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-600 cursor-pointer"
-        >
-          Add to Favorites
-        </button>
-      )}
-      {onRemoveFavorite && (
-        <button
-          onClick={() => onRemoveFavorite(pokemon)}
-          className="hover:underline mt-2 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 cursor-pointer"
-        >
-          Remove from Favorites
-        </button>
-      )}
+      <Link to={`/pokemon/${pokemon.id}`} className="text-center">
+        <img
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+          className="w-32 h-32"
+        />
+        <h2 className="capitalize mt-2 font-bold">{pokemon.name}</h2>
+      </Link>
+      <div className="flex gap-2 mt-1">
+        {onAddFavorite && (
+          <button
+            onClick={() => onAddFavorite(pokemon)}
+            className="text-xs hover:underline mt-2 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-600 cursor-pointer"
+          >
+            Add to Favorites
+          </button>
+        )}
+        {onRemoveFavorite && (
+          <button
+            onClick={() => onRemoveFavorite(pokemon)}
+            className="text-xs hover:underline mt-2 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 cursor-pointer"
+          >
+            x
+          </button>
+        )}
+      </div>
     </div>
   );
 };
