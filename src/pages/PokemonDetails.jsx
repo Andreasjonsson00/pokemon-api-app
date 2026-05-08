eca50a69cc9731b38f5c72f5a7dcd4cf37d6ff97
Andreas <andreasjonsson00@gmail.com>
2026-05-08 11:36:16 +0200

    add DeleteButton component

--- src/pages/PokemonDetails.jsx
+++ src/pages/PokemonDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getPokemonById } from "../api/dataApi";
 import FavoriteButton from "../components/FavoriteButton";
+import DeleteButton from "../components/DeleteButton";
 
 const PokemonDetails = ({ onAddFavorite, onRemoveFavorite }) => {
   const { id } = useParams();
@@ -65,12 +66,10 @@ const PokemonDetails = ({ onAddFavorite, onRemoveFavorite }) => {
           )}
 
           {onRemoveFavorite && (
-            <button
-              onClick={() => onRemoveFavorite(pokemon)}
-              className="text-xs hover:underline mt-2 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 hover:shadow-lg active:scale-90 active:-rotate-1 transition-all duration-150 cursor-pointer"
-            >
-              x
-            </button>
+            <DeleteButton
+              onRemoveFavorite={onRemoveFavorite}
+              pokemon={pokemon}
+            />
           )}
         </div>
       </div>
