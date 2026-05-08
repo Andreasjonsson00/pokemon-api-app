67fde64027e7e315a1de64cec52c82857e390f88
Andreas <andreasjonsson00@gmail.com>
2026-05-08 12:34:05 +0200

    pin buttons to card bottom

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -4,10 +4,10 @@ import DeleteButton from "./DeleteButton";
 
 const Pokemon = ({ pokemon, onAddFavorite, onRemoveFavorite, children }) => {
   return (
-    <div className="w-64 min-h-[250px] p-4 border rounded-lg bg-white shadow-md flex flex-col items-center justify-between transition transform hover:scale-105 hover:shadow-xl">
+    <div className="flex min-h-[360px] w-64 flex-col rounded-lg border bg-white p-4 shadow-md transition transform hover:scale-105 hover:shadow-xl">
       <Link
         to={`/pokemon/${pokemon.id}`}
-        className="text-center flex flex-col items-center"
+        className="flex flex-1 flex-col items-center text-center"
       >
         <img
           src={pokemon.sprites.front_default}
@@ -21,15 +21,17 @@ const Pokemon = ({ pokemon, onAddFavorite, onRemoveFavorite, children }) => {
           </p>
         ) : null}
       </Link>
+      <div className="mt-auto">
         {children}
-      <div className="flex gap-2 mt-1">
-        {onAddFavorite && (
-          <FavoriteButton onAddFavorite={onAddFavorite} pokemon={pokemon} />
-        )}
+        <div className="mt-3 flex flex-col items-center gap-2">
+          {onAddFavorite && (
+            <FavoriteButton onAddFavorite={onAddFavorite} pokemon={pokemon} />
+          )}
+          {onRemoveFavorite && (
+            <DeleteButton onRemoveFavorite={onRemoveFavorite} pokemon={pokemon} />
+          )}
+        </div>
       </div>
-      {onRemoveFavorite && (
-        <DeleteButton onRemoveFavorite={onRemoveFavorite} pokemon={pokemon} />
-      )}
     </div>
   );
 };
